180f0b38932a25b93d5e1410cfdadbf740a54946
acerveaux <acerveaux@botdesign.net>
2020-10-18 10:17:16 +0200

    Add new properties

--- src/scripts/objects/characters/types/character.ts
+++ src/scripts/objects/characters/types/character.ts
@@ -2,24 +2,28 @@ import { Item } from '../../items'
 import { CharacterActions } from './actions'
 import { Direction, State, StatusEffect } from './enums'
 
-export interface Character {
-  life: number
-  maxLife: number
+export interface Character extends CharacterActions {
+  life: {
+    heart: number
+    max: number
+    extra: number
+  }
   speed: number
   attackCost: number
-  defense: number
+  attackSpeed: number
+  defenseCost: number
+  luck: number
   effect: Effect[]
   inventory: Inventory
   direction: Direction
-  actions: CharacterActions
   state: State
 }
-
 export interface Inventory {
   items: Item[]
   activeItem: Item
   trinket: Item
   consumable: Item
+  coins: number
 }
 
 export interface Effect {
