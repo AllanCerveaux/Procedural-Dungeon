58d2cdfaf0a149765743a31281433aa764f71558
Callan <cerveauxallanjean@gmail.com>
2022-07-23 17:54:37 +0200

    Add some Constant

--- src/scripts/constants.ts
+++ src/scripts/constants.ts
@@ -1,4 +1,22 @@
-const DEFAULT_WIDTH = 1280
-const DEFAULT_HEIGHT = 720
-
-export { DEFAULT_WIDTH, DEFAULT_HEIGHT }
+export const DEFAULT_PHYSICS: 'matter' | 'arcade' = 'arcade'
+export const DEFAULT_WIDTH = 1280
+export const DEFAULT_HEIGHT = 720
+export const DEFAULT_INPUT_KEYS = {
+  left: Phaser.Input.Keyboard.KeyCodes['LEFT'],
+  right: Phaser.Input.Keyboard.KeyCodes['RIGHT'],
+  up: Phaser.Input.Keyboard.KeyCodes['UP'],
+  down: Phaser.Input.Keyboard.KeyCodes['DOWN'],
+  fire: Phaser.Input.Keyboard.KeyCodes['X'],
+  activeItem: Phaser.Input.Keyboard.KeyCodes['SPACE'],
+  supportItem: Phaser.Input.Keyboard.KeyCodes['W'],
+  fullscreen: Phaser.Input.Keyboard.KeyCodes['F'],
+  pause: Phaser.Input.Keyboard.KeyCodes['P']
+}
+export enum SCENES {
+  PRELOAD = 'preload-scene',
+  MENU = 'menu-scene',
+  OPTION = 'option-scene',
+  SELECT_HERO = 'select-hero-scene',
+  MAIN = 'main-scene',
+  GAME_OVER = 'game-over-scene'
+}
