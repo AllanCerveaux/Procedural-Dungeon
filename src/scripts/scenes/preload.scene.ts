e5943f18899153a47168a94af4299114b264e16c
Callan <cerveauxallanjean@gmail.com>
2022-07-23 17:56:01 +0200

    Load sprite and change key to constant

--- src/scripts/scenes/preload.scene.ts
+++ src/scripts/scenes/preload.scene.ts
@@ -1,8 +1,11 @@
+import { SCENES } from '@constants'
 export class PreloadScene extends Phaser.Scene {
   constructor() {
-    super({ key: 'preload-scene' })
+    super({ key: SCENES.PRELOAD })
   }
-
+  preload() {
+    this.load.image('cube', 'cube_test.png')
+  }
   create() {
     console.log(this.scene.key)
     this.scene.start('main-scene')
