f60b1c94554759071539da4d856943afefeea504
Callan <cerveauxallanjean@gmail.com>
2022-07-26 16:12:49 +0200

    Can increase and decrease heart

--- src/scripts/scenes/overlay/hud.scene.ts
+++ src/scripts/scenes/overlay/hud.scene.ts
@@ -1,18 +1,21 @@
+import { HEART, OVERLAY } from '@constants'
+
 import { Heart } from '@objects/hud/heart'
-import { OVERLAY } from '@constants'
 
 export class HUDScene extends Phaser.Scene {
   life_bar: Heart[]
-
   life: {
     heart: number
     extra: number
     max: number
   }
+
   heart_key: any
   extra_key: any
   remove_heart: any
   remove_extra: any
+  decrease_key: any
+  increase_key: any
   constructor() {
     super({ key: OVERLAY.HUD })
     this.life_bar = []
@@ -25,15 +28,17 @@ export class HUDScene extends Phaser.Scene {
     this.remove_heart = this.input.keyboard.addKey('f')
     this.extra_key = this.input.keyboard.addKey('e')
     this.remove_extra = this.input.keyboard.addKey('r')
+    this.decrease_key = this.input.keyboard.addKey('z')
+    this.increase_key = this.input.keyboard.addKey('a')
   }
 
   update(time: number, delta: number): void {
     if (Phaser.Input.Keyboard.JustDown(this.heart_key)) {
-      this.addHeart(false, 0)
+      this.addHeart(false, HEART.FULL)
       console.log(this.life_bar)
     }
     if (Phaser.Input.Keyboard.JustDown(this.extra_key)) {
-      this.addHeart(true, 2)
+      this.addHeart(true, HEART.FULL)
       console.log(this.life_bar)
     }
     if (Phaser.Input.Keyboard.JustDown(this.remove_heart)) {
@@ -42,6 +47,15 @@ export class HUDScene extends Phaser.Scene {
     }
     if (Phaser.Input.Keyboard.JustDown(this.remove_extra)) {
       this.removeHeart(true)
+      console.log(this.life_bar)
+    }
+
+    if (Phaser.Input.Keyboard.JustDown(this.decrease_key)) {
+      this.lifeDown()
+      console.log(this.life_bar)
+    }
+    if (Phaser.Input.Keyboard.JustDown(this.increase_key)) {
+      this.lifeUp()
       console.log(this.life_bar)
     }
   }
@@ -58,6 +72,7 @@ export class HUDScene extends Phaser.Scene {
     if (!isExtra && extra.length > 0 && this.life_bar.length * 2 >= this.life.max) this.removeHeart(true)
     if (this.life_bar.length * 2 >= this.life.max) return
 
+    // @TODO: CAN REFACTO
     if (!isExtra) {
       x = (hearts.at(-1)?.x || 0) >= 150 ? 30 : (hearts.at(-1)?.x || 0) + 30
       y = (hearts.at(-1)?.x || 0) >= 150 ? 50 : hearts.at(-1)?.y || 20
@@ -91,6 +106,35 @@ export class HUDScene extends Phaser.Scene {
       rmElm.destroy()
       const rmElmIndex = this.life_bar.indexOf(rmElm)
       this.life_bar = [...this.life_bar.filter((_, i) => i !== rmElmIndex)]
+    }
+  }
+
+  lifeDown() {
+    const { hearts, extra } = this.hearts()
+    const last_extra = extra.at(-1)
+    if (last_extra) {
+      last_extra.decreaseState()
+      if (last_extra.isEmpty()) this.removeHeart(true)
+    } else {
+      hearts
+        .filter(heart => !heart.isEmpty())
+        .at(-1)
+        ?.decreaseState()
+    }
+  }
+
+  lifeUp() {
+    const { hearts, extra } = this.hearts()
+    if (extra.length > 0) {
+      extra
+        .filter(extra => extra.state === HEART.HALF)
+        .at(0)
+        ?.increaseState()
+    } else {
+      hearts
+        .filter(heart => heart.isEmpty() || heart.state === HEART.HALF)
+        .at(0)
+        ?.increaseState()
     }
   }
 
