fdbbc90186dea3fd5af1c8e0171f4ab66b7b0241
Callan <cerveauxallanjean@gmail.com>
2022-07-31 19:13:04 +0200

    Add class for all heroes

--- src/scripts/objects/heroes/Elf.ts
+++ src/scripts/objects/heroes/Elf.ts
@@ -1,14 +1,15 @@
 import Player from '@objects/player/Player'
 
-export class Knight extends Player {
+export class Elf extends Player {
   constructor(scene: Phaser.Scene, x: number, y: number, gender: string) {
-    const name: string = `knight_${gender}`
+    const name: string = `elf_${gender}`
     const max_speed: number = 200
 
     super(scene, x, y, { key: 'characters', name, max_speed })
-    this.life.heart = 8
+
+    this.life.heart = 4
     this.life.extra = 0
-    this.speed = 130
+    this.speed = 180
     this.attack_cost = 2
     this.attack_speed = 1800
     this.defense_cost = 1
--- src/scripts/objects/heroes/Lizard.ts
+++ src/scripts/objects/heroes/Lizard.ts
@@ -1,14 +1,15 @@
 import Player from '@objects/player/Player'
 
-export class Knight extends Player {
+export class Lizard extends Player {
   constructor(scene: Phaser.Scene, x: number, y: number, gender: string) {
-    const name: string = `knight_${gender}`
+    const name: string = `lizard_${gender}`
     const max_speed: number = 200
 
     super(scene, x, y, { key: 'characters', name, max_speed })
-    this.life.heart = 8
-    this.life.extra = 0
-    this.speed = 130
+
+    this.life.heart = 0
+    this.life.extra = 6
+    this.speed = 90
     this.attack_cost = 2
     this.attack_speed = 1800
     this.defense_cost = 1
