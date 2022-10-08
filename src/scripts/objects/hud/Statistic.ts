395f68cff0efb746930bbac6cd1f568def0648ec
Allan <allan.cerveaux@solotech.com>
2022-10-08 17:37:23 +0200

    Improve way to update statistic player

--- src/scripts/objects/hud/Statistic.ts
+++ src/scripts/objects/hud/Statistic.ts
@@ -1,7 +1,18 @@
+import Base from "@objects/entities/Base";
+
+type StatisticOption = {
+  entity: Base,
+  statName: string
+} 
+
 export default class Statistic extends Phaser.GameObjects.Text {
-  statName: string | undefined
-  value: number | undefined
-  constructor(scene: Phaser.Scene, x: number, y: number, text?: string, style?: Phaser.Types.GameObjects.Text.TextStyle, statName?: string, value?: number) {
+  statName: string
+  value: number
+  entity: Base
+  constructor(scene: Phaser.Scene, x: number, y: number, text: string, style: Phaser.Types.GameObjects.Text.TextStyle, {
+    entity,
+    statName
+  }: StatisticOption) {
     super(scene, x, y, text || '', style || {
       fontFamily: '"Press Start 2P"',
       fontSize: '16px',
@@ -9,9 +20,21 @@ export default class Statistic extends Phaser.GameObjects.Text {
       align: 'right'
     })
     this.statName = statName
-    this.value = value
-    
-    this.setText(`${statName}: ${value as number / 100}`)
+    this.entity = entity
+    this.setText(`${statName}: ${entity.statistics[statName] / 100}`)
   }
 
+  statUP(value: number) {
+    this.value += value 
+  }
+  
+  statDOWN(value: number) {
+    this.value -= value
+  }
+  
+  updateStat() {
+    console.log(this.value, this.entity.statistics)
+    this.setText(`${this.statName}: ${this.entity.statistics[this.statName] / 100}`)
+    // this[`stat${state}`](value)
+  }
 }
