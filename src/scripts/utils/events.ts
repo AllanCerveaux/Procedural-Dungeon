f887b800cb1595773b612eac4e4f818e77b8fe24
Allan <allan.cerveaux@solotech.com>
2022-10-16 16:58:49 +0200

    Add global emmiter

--- src/scripts/utils/events.ts
+++ src/scripts/utils/events.ts
@@ -1,1 +1,2 @@
-export const PlayerEmitter = new Phaser.Events.EventEmitter();
+export const PlayerEmitter = new Phaser.Events.EventEmitter()
+export const GlobalEmitter = new Phaser.Events.EventEmitter()
