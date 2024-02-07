d2ae25e241de8309c109c47e0bb90b80baca8e8a
Allan <allan.cerveaux@solotech.com>
2024-02-07 23:09:39 +0100

    lint config

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,17 @@
-import { defineConfig } from 'vite';
-import eslint from 'vite-plugin-eslint';
-import tsconfigPaths from 'vite-tsconfig-paths';
+import { defineConfig } from 'vite'
+import eslint from 'vite-plugin-eslint'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
-  plugins: [eslint(), tsconfigPaths()],
-  resolve: {},
-  build: {
-    // phaser doesn't accept inlined assets
-    assetsInlineLimit: 0
-  },
-  server: {
-    host: process.env.VITE_HOST ? process.env.VITE_HOST : '0.0.0.0',
-    port: 3000,
-    open: '/'
-  }
-});
+	plugins: [eslint(), tsconfigPaths()],
+	resolve: {},
+	build: {
+		// phaser doesn't accept inlined assets
+		assetsInlineLimit: 0,
+	},
+	server: {
+		host: process.env.VITE_HOST ? process.env.VITE_HOST : '0.0.0.0',
+		port: 3000,
+		open: '/',
+	},
+})
