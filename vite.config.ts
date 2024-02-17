8ab01ea1ffd0dcc0f3b9291b3f420cd71ba7cdec
Allan <allan.cerveaux@solotech.com>
2024-02-17 22:50:54 +0100

    Add config for deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
 	plugins: [eslint(), tsconfigPaths()],
-	resolve: {},
+	base: '/Procedural-Dungeon/',
 	build: {
 		// phaser doesn't accept inlined assets
 		assetsInlineLimit: 0,
